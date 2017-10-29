d3b21f5e0c61b8df75d36964f2d3f83d156d4cad
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-10-29 13:54:43 +0000

    First Firebase Function set up

--- functions/index.js
+++ functions/index.js
@@ -8,96 +8,180 @@
 
 
 
-
+const merge = require('merge')
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-
+const functions = require('firebase-functions')
 // The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-var db = admin.firestore();
-
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  const first = req.query.first;
-  const last = req.query.last;
-  // // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  // admin.database().ref('/messages').push({original: original}).then(snapshot => {
-  //   // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-  //   res.redirect(303, snapshot.ref);
-  // });
-  var docRef = db.collection('users').doc('alovelace');
-
-  var setAda = docRef.set({
-      first: first,
-      last: last,
-      born: 1815
-  }).then(snapshot => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.status(200).json(snapshot.ref);
-  })
-});
-
-
-
-
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
+var db = admin.firestore()
 
 exports.saveCard = functions.https.onRequest((req, res) => {
-  const data = {
-    organisationID: req.data.organisation,
-    cardID: req.data.objectID,
-    content: req.data.content,
-    authorID: req.data.author || req.data.sender
+  try {
+    console.log('📬 Data just arrived:', req.body)
+    requireProps(req.body, ['organisationID', 'content', 'authorID'], [['objectID', 'teamID']])
+    const data = req.body; data.cardID = req.body.objectID; if (data.objectID) delete data.objectID
+
+    getUser(data.organisationID, data.authorID)
+    .then(user => {
+      data.user = user.data
+      return getCard(data.organisationID, data.cardID)
+    }).catch(function(e) {
+      return new Promise((res, rej) => { res() })
+    }).then(card => {
+      data.card = card ? card.data : {
+        content: data.content,
+        teams: [ getTeamRef(data.teamID) ],
+      }
+      const updatedCard = updateCardContent(data.organisationID, data.user, data.card, data.content)
+      if (card) {
+        data.cardRef = card.ref
+        return data.cardRef.set(updatedCard)
+      } else if (data.cardID) {
+        return getCardRef(data.organisationID, data.cardID).set(updatedCard)
+      } else {
+        return getCardsRef(data.organisationID).add(updatedCard)
+      }
+    }).then(snapshot => {
+      if (!data.cardID) data.cardID = snapshot._referencePath.segments[snapshot._referencePath.segments.length - 1]
+      return getCardRef(data.organisationID, data.cardID).get()
+    }).then(doc => {
+      const cardData = merge(doc.data(), { objectID: data.cardID })
+      console.log('📂 Card Saved!', cardData)
+      res.status(200).json(cardData)
+    }).catch(e => { console.log('📛 Error!', e); res.status(500).send(e) })
+  } catch (e) {
+    console.log('📛 Error!', e.status, e.message)
+    res.status(e.status || 500).send(e.message || 'Unknown Server Error')
   }
+})
 
-  getUser(data.organisationID, data.authorID)
-  .then(user => { data.user = user.data; return getCard(data.organisationID, data.cardID) })
-  .then(card => {
-    data.card = card.data
-    data.cardRef = card.ref
-    const newCard = updateCardContent(data.organisationID, data.user, data.card, data.content)
-    return data.cardRef.set(newCard)
-  }).then(snapshot => {
-    res.status(200).json(snapshot.ref)
-  }).catch(e => res.error(e))
-});
+exports.changeUserTeamRole = functions.https.onRequest((req, res) => {
+  try {
+    console.log('📬 Data just arrived:', req.body)
+    requireProps(req.body, ['organisationID', 'userID', 'teamID', 'role'])
+    const data = req.body
 
-const getObject = function(organisationID, collectionID, objectID) {
+    getUser(data.organisationID, data.userID)
+    .then(user => { data.user = user.data; data.userRef = user.ref; return getTeam(data.organisationID, data.teamID) })
+    .then(team => {
+      data.team = team.data
+      data.teamRef = team.ref
+      const updatedUser = updateUserTeamRole(data.organisationID, data.user, data.team, data.teamID, data.teamRef, data.role)
+      return data.userRef.set(updatedUser)
+    }).then(snapshot => {
+      return admin.auth().setCustomUserClaims(data.userID, {admin: true})
+    }).then(snapshot => {
+      res.status(200).json(snapshot)
+    }).catch(e => { console.log('📛 Error!', e); res.status(500).send(e) })
+  } catch (e) {
+    console.log('📛 Error!', e)
+    res.status(e.status || 500).send(e.message || 'Unknown Server Error')
+  }
+})
+
+
+
+
+
+
+const getDoc = function(organisationID, collectionID, docID) {
   return new Promise((resolve, reject) => {
-    const ref = db.collection('organisations').doc(organisationID).collection(collectionID).doc(userID)
+    const ref = db.collection('organisations').doc(organisationID).collection(collectionID).doc(docID)
     ref.get().then(doc => {
       if (!doc.exists) {
-        console.log('❌ Couldn\'t find object from ' + objectType)
+        console.log('❌ Couldn\'t find object from ' + collectionID)
         reject(404)
       } else {
-        console.log('🔍 Object from ' + objectType + ' found:', ref, doc.data());
+        console.log('🔍 Object from ' + collectionID + ' found:', /* ref, */ doc.data())
         resolve({ data: doc.data(), ref: ref })
       }
     })
   })
 }
 
+const getDocRef = function(organisationID, collectionID, docID) {
+  return getCollectionRef(organisationID, collectionID).doc(docID)
+}
+const getCollectionRef = function(organisationID, collectionID) {
+  return db.collection('organisations').doc(organisationID).collection(collectionID)
+}
+
 const getUser = function(organisationID, userID) {
-  return getObject(organisationID, 'users', userID)
+  return getDoc(organisationID, 'users', userID)
 }
-
 const getTeam = function(organisationID, teamID) {
-  return getObject(organisationID, 'teams', teamID)
+  return getDoc(organisationID, 'teams', teamID)
+}
+const getCard = function(organisationID, cardID) {
+  return getDoc(organisationID, 'cards', cardID)
+}
+const getUserRef = function(organisationID, userID) {
+  return getDocRef(organisationID, 'users', userID)
+}
+const getTeamRef = function(organisationID, teamID) {
+  return getDocRef(organisationID, 'teams', teamID)
+}
+const getCardRef = function(organisationID, cardID) {
+  return getDocRef(organisationID, 'cards', cardID)
+}
+const getUsersRef = function(organisationID) {
+  return getCollectionRef(organisationID, 'users')
+}
+const getTeamsRef = function(organisationID) {
+  return getCollectionRef(organisationID, 'teams')
+}
+const getCardsRef = function(organisationID) {
+  return getCollectionRef(organisationID, 'cards')
 }
 
-const getCard = function(organisationID, cardID) {
-  return getObject(organisationID, 'cards', cardID)
+const updateCardContent = function(organisationID, user, card, content) {
+  const permission = getPermissionLevel(organisationID, user, card)
+  console.log('🔑 Permission level: ' + permission)
+  switch (permission) {
+    case 'write':
+      card.content = content // Should we merge here instead of replacing?
+      break
+    case 'submit':
+      card.pending ? card.pending.push(content) : card.pending = [content] // Should we merge here instead of replacing?
+      break
+    default:
+      // Something here?
+  }
+  console.log('🗂 Final card: ', card)
+  return card
+}
+
+const updateUserTeamRole = function(organisationID, user, team, teamID, teamRef, role) {
+  var updated = false
+  user.teams = user.teams.map(function(userTeam) {
+    console.log(userTeam.team._referencePath.segments[userTeam.team._referencePath.segments.length - 1], teamID)
+    if (userTeam.team._referencePath.segments[userTeam.team._referencePath.segments.length - 1] == teamID) {
+      userTeam.role = role
+      updated = true
+    }
+    return userTeam
+  })
+  console.log(updated)
+  if (!updated) {
+    user.teams.push({ team: teamRef, role: role })
+  }
+  console.log('👤 Updated user:', user)
+  return user
 }
 
 const getPermissionLevel = function(organisationID, user, card) {
-  const roles = user.teams.filter(function(team) {
-    return card.teams.indexOf(team.team) > -1
-  }).map(function(team) {
-    return team.role
+  console.log('📃👩‍👩‍👦‍👦  Card Teams: ', card.teams)
+  console.log('👤👩‍👩‍👦‍👦  User Teams: ', user.teams)
+  const roles = user.teams.filter(function(userTeam) {
+    return card.teams.filter(function(cardTeam) {
+      return compareReferences(cardTeam, userTeam.team)
+    }).length > 0
+  }).map(function(userTeam) {
+    console.log(userTeam);
+    return userTeam.role
   })
+  console.log('🎓 Roles: ', roles)
   if (roles.indexOf('manager') > -1)
     return 'write'
   else if (roles.indexOf('member') > -1)
@@ -106,19 +190,24 @@ const getPermissionLevel = function(organisationID, user, card) {
     return null
 }
 
-const updateCardContent = function(organisationID, user, card, content) {
-  const permission = getPermissionLevel(organisationID, user, card)
-  console.log('🔑 Permission level: ' + permission);
-  switch (permission) {
-    case 'write':
-      card.content = content // Should we merge here instead of replacing?
-      break;
-    case 'read':
-      card.pending.push(content) // Should we merge here instead of replacing?
-      break;
-    default:
-      // Something here?
-  }
-  console.log('🗂 Final card: ', card);
-  return card
+const compareReferences = function(ref1, ref2) {
+  console.log(ref1._referencePath.segments.join('/'), ref2._referencePath.segments.join('/'));
+  return ref1._referencePath.segments.join('/') == ref2._referencePath.segments.join('/') // Should check _firestore as well?
+}
+
+const requireProps = function(data, props, orProps) {
+  props.forEach(function(key) {
+    if (!data[key]) {
+      const e = { status: 400, message: 'No ' + key + ' data found in request.' }
+      throw new Error(e)
+    }
+  })
+  orProps.forEach(function(group) { // These are groups of properties where at least one in the group must be present
+    if (group.filter(function(key) {
+      return !!data[key]
+    }).length == 0) {
+      const e = { status: 400, message: 'No ' + key + ' data found in request.' }
+      throw new Error(e)
+    }
+  })
 }
