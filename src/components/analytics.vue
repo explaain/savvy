ef66a547abe1a93fa5c2eb63e7a11030b6f1349c
nickplatt <np326@exeter.ac.uk>
2017-11-06 20:23:55 +0000

    slight tweak to graph

--- src/components/analytics.vue
+++ src/components/analytics.vue
@@ -1,33 +1,29 @@
 <template>
-  <div id="Analytics">
-    <h1>{{msg}}</h1>
+<div id="Analytics">
+  <h1>{{msg}}</h1>
 
-    <div class="graph-grid">
-      <div class="column">
-        <div class="graph-card">
-          <h4>This graph shows stuff</h4>
-          <exampleline class="wrapper" :data="data" :options="options"></exampleline>
-        </div>
+  <div class="graph-grid">
+    <div class="column">
+      <div class="graph-card">
+        <h4>This graph shows stuff</h4>
+        <exampleline class="wrapper" :data="data" :options="options"></exampleline>
       </div>
-      <div class="column">
-        <div class="graph-card">
-          <h4>This graph shows other stuff</h4>
-          <exampleline class="wrapper" :data="data" :options="options" ></exampleline>
-        </div>
+    </div>
+    <div class="column">
+      <div class="graph-card">
+        <h4>This graph shows other stuff</h4>
+        <exampleline class="wrapper" :data="data" :options="options"></exampleline>
       </div>
-      <div class="column">
-        <div class="graph-card">
-          <h4>This graph shows even more other stuff</h4>
-          <exampleline class="wrapper" :data="data" :options="options"></exampleline>
-        </div>
-      </div>
-        <div class="graph-card">
-          <h4>This graph shows even more other stuff</h4>
+    </div>
+    <div class="column">
+      <div class="graph-card">
+        <h4>This graph shows even more other stuff</h4>
         <examplebar class="wrapper" :data="dataBar" :options="optionsBar"></examplebar>
       </div>
     </div>
-
   </div>
+
+</div>
 </template>
 
 <script>
@@ -36,29 +32,25 @@ import BarChart from './charts/bar-chart'
 
 export default {
   name: 'Analytics',
-  data () {
+  data() {
     return {
       msg: 'Welcome analytics',
       data: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        datasets: [
-          {
-            label: 'GitHub Commits',
-            backgroundColor: '#f87979',
-            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-          }
-        ]
+        datasets: [{
+          label: 'GitHub Commits',
+          backgroundColor: '#f87979',
+          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+        }]
       },
       options: {},
       dataBar: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        datasets: [
-          {
-            label: 'Testing Commits',
-            backgroundColor: '#f43759',
-            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-          }
-        ]
+        datasets: [{
+          label: 'Testing Commits',
+          backgroundColor: '#f43759',
+          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+        }]
       },
       optionsBar: {}
     }
