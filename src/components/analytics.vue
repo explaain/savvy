d561c9ca00fe965a6bebb3124006e1f31f11816a
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-11-12 19:43:00 +0000

    All packaged nicely into chart.vue

--- src/components/analytics.vue
+++ src/components/analytics.vue
@@ -1,26 +1,17 @@
 <template>
   <div id="Analytics">
     <h1>{{msg}}</h1>
-
     <div class="graph-grid">
       <div class="column">
         <div class="graph-card">
           <h4>This graph shows stuff</h4>
-          <barchart class="wrapper" :chartData="dataBar" :options="optionsBar"></barchart>
+          <chart class="wrapper" :type="'bar-chart'" :options="{}" :query="{start: '2017-11-05', end: '2017-11-11', event: 'Card Saved'}"></chart>
         </div>
       </div>
       <div class="column">
         <div class="graph-card">
           <h4>This graph shows other stuff</h4>
-          <barchart class="wrapper" :chartData="dataBar" :options="optionsBar"></barchart>
-        </div>
-      </div>
-      <div class="column">
-        <div class="graph-card">
-          <h4>This graph shows even more other stuff</h4>
-          <!--<examplebar class="wrapper" :chartData="dataBar" :options="optionsBar"></examplebar> -->
-          <component v-bind:is="chartType" :chartData="dataLine" :options="optionsLine"></component>
-          <button v-on:click="fetchData('2017-11-05', '2017-11-11', 'Card Saved')">Get data!</button>
+          <chart class="wrapper" :type="'line-chart'" :options="{}" :query="{start: '2017-11-03', end: '2017-11-07', event: 'Card Saved'}"></chart>
         </div>
       </div>
     </div>
@@ -32,8 +23,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import LineChart from './charts/line-chart'
-import BarChart from './charts/bar-chart'
+import Chart from './charts/chart'
 
 Vue.use(VueAxios, axios)
 
@@ -41,49 +31,11 @@ export default {
   name: 'Analytics',
   data() {
     return {
-      msg: 'Welcome analytics',
-      dataLine: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        datasets: [{
-          label: 'GitHub Commits',
-          backgroundColor: '#f87979',
-          data: []
-        }]
-      },
-      optionsLine: {},
-      dataBar: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        datasets: [{
-          label: 'Testing Commits',
-          backgroundColor: '#f43759',
-          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-        }]
-      },
-      optionsBar: {},
-      chartType: 'linechart'
+      msg: 'Welcome analytics'
     }
   },
   components: {
-    linechart: LineChart,
-    barchart: BarChart
-  },
-  methods: {
-    fetchData: function(start, end, event) {
-      const url = 'http://localhost:3000/analytics/fetch'
-      const data = {
-        start: start,
-        end: end,
-        event: event
-      }
-      Vue.axios.post(url, data)
-      .then((response) => {
-        console.log(response)
-        console.log(this.$data.dataLine.datasets.data)
-        this.$data.dataLine.datasets.data = response.data.data.value
-      }).catch(function(error) {
-        console.log(error)
-      })
-    }
+    chart: Chart
   }
 }
 </script>
---
+++ src/components/charts/chart.vue
@@ -0,0 +1,66 @@
+<template>
+  <component v-bind:is="type" :chartData="data" :options="options"></component>
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import LineChart from './line-chart'
+import BarChart from './bar-chart'
+
+Vue.use(VueAxios, axios)
+
+export default {
+  name: 'Chart',
+  props: [
+    'type',
+    'query',
+    'options'
+  ],
+  data() {
+    return {
+      data: {}
+    }
+  },
+  components: {
+    'line-chart': LineChart,
+    'bar-chart': BarChart
+  },
+  created: function() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData: function() {
+      const self = this
+      const url = 'http://localhost:3000/analytics/fetch'
+      console.log(url)
+      console.log(self.query)
+      Vue.axios.post(url, self.query)
+      .then((response) => {
+        const data = response.data[self.query.event]
+        console.log(data)
+        self.data = {
+          labels: Object.keys(data),
+          datasets: [{
+            label: self.query.event,
+            backgroundColor: '#f43759',
+            data: Object.keys(data).map(key => data[key])
+          }]
+        }
+      }).catch(function(error) {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+.wrapper {
+  width: 100%;
+  height: 80%;
+  margin: 0px auto;
+}
+
+</style>
