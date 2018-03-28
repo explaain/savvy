99c787aad982c018e69643fec009d482d9264b51
Jeremy Evans <jeremy.n.evans@gmail.com>
2018-03-28 15:12:40 +0100

    Undone demo!

--- src/chrome-sidebar.js
+++ src/chrome-sidebar.js
@@ -1,14 +1,14 @@
-// import Main from './main.js'
-//
-// const props = {
-//   plugin: true,
-//   sidebar: true
-// }
-//
-// console.log('Running Sidebar')
-//
-// const main = new Main(props)
-//
-// console.log('main:', main)
+import Main from './main.js'
+
+const props = {
+  plugin: true,
+  sidebar: true
+}
+
+console.log('Running Sidebar')
+
+const main = new Main(props)
+
+console.log('main:', main)
 
 // @TODO: Uncomment everything when we're no longer doing demo!
