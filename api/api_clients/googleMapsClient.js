87c02995f87c5a82c1c3f7439fa7090ce44b4b16
Jeremy Evans <jeremy.n.evans@gmail.com>
2018-04-11 18:07:12 +0100

    Removed /api folder

--- api/api_clients/googleMapsClient.js
+++
@@ -1,7 +0,0 @@
-var properties = require('../config/properties.js');
-
-const googleMapsClient = require('@google/maps').createClient({
-  key: properties.google_maps_api
-});
-
-module.exports = googleMapsClient;
